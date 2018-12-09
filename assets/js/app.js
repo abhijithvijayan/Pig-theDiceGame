659953673b708dada66e15214cfc3802a6142012
abhijithvijayan <34790378+abhijithvijayan@users.noreply.github.com>
2018-12-09 14:10:45 +0530

    arrow functions

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,7 @@
 var scores, currentPlayer, currentScore;
 
-function init() {
+// initialization
+var init = () => {
   scores = [0, 0];
   currentScore = 0;
   currentPlayer = 0;
@@ -21,31 +22,19 @@ function init() {
   //setting 1st player as active player
   document.querySelector(".player-0").classList.add("active");
   document.querySelector(".pCurrent-0").classList.add("current");
-
-  // document.querySelector('#score-' + currentPlayer).textContent = dice;
-  // document.querySelector('#score-' + currentPlayer).innerHTML = "<em>" + dice + "</em";
-  // var x = document.querySelector('#score-' + currentPlayer).textContent;
-  // console.log(x);
-  
   // show buttons
   document.querySelector(".roll-btn").style.display = "block";
   document.querySelector(".hold-btn").style.display = "block";
-}
+};
 init();
-
-/*
-function btnClick() {
-    // do something
-}
-btnClick();
-
-document.querySelector('.roll-btn').addEventListener('click', btnClick);            // this will call btnclick() which will then call the function
-*/
 
 // EVENT LISTENERS
 
-// ROLL BUTTON
-document.querySelector(".roll-btn").addEventListener("click", function() {
+// 1. NEW GAME BUTTON
+document.querySelector(".new-btn").addEventListener("click", init); // this will call init()
+
+// 2. ROLL BUTTON
+document.querySelector(".roll-btn").addEventListener("click", () => {
   //anonymous fn - that cannot be reused
   // 1. Random Number
   var diceNum = Math.floor(Math.random() * 6 + 1);
@@ -66,8 +55,8 @@ document.querySelector(".roll-btn").addEventListener("click", function() {
   }
 });
 
-// HOLD BUTTON
-document.querySelector(".hold-btn").addEventListener("click", function() {
+// 3. HOLD BUTTON
+document.querySelector(".hold-btn").addEventListener("click", () => {
   // 1. Add score to total
   scores[currentPlayer] += currentScore;
 
@@ -88,37 +77,37 @@ document.querySelector(".hold-btn").addEventListener("click", function() {
   }
 });
 
-// NEW GAME BUTTON
-document.querySelector(".new-btn").addEventListener("click", init); // this will call init()
-
-function playerSwitch() {
+// Player Switch Function
+var playerSwitch = () => {
   currentScore = 0;
   document.getElementById("score-" + currentPlayer).textContent = currentScore;
-  // active class injection and removal
 
-  // document.querySelector('.player-' + currentPlayer).classList.remove('active');
+  // active class injection and removal
   document.querySelector(".player-" + currentPlayer).classList.toggle("active");
-  document
-    .querySelector(".pCurrent-" + currentPlayer)
-    .classList.remove("current"); // the block color
+  document.querySelector(".pCurrent-" + currentPlayer).classList.remove("current"); // the block color
 
   currentPlayer === 0 ? (currentPlayer = 1) : (currentPlayer = 0);
 
-  // document.querySelector('.player-' + currentPlayer).classList.add('active');
   document.querySelector(".player-" + currentPlayer).classList.toggle("active");
   document.querySelector(".pCurrent-" + currentPlayer).classList.add("current"); // the block color
 
   // hide the dice
   document.querySelector(".dice").style.display = "none";
-}
+};
 
 // OVERLAY MENU
-document.querySelector(".overlay").addEventListener("click", overlayON);
-function overlayON() {
-  document.getElementById("overlay").style.display = "block";
-}
+var overlaySwitch = value => {
+  // eq. to : var overlaySwitch = function(value)
+  if (value) {
+    document.getElementById("overlay").style.display = "block";
+  } else {
+    document.getElementById("overlay").style.display = "none";
+  }
+};
 
-document.querySelector("#overlay").addEventListener("click", overlayOFF);
-function overlayOFF() {
-  document.getElementById("overlay").style.display = "none";
-}
+document.querySelector(".overlay-btn").addEventListener("click", () => {
+  overlaySwitch(1);
+});
+document.querySelector("#overlay").addEventListener("click", () => {
+  overlaySwitch(0);
+});
